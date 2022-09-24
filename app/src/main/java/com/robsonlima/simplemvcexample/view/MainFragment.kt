b38c93c0e7db0b928693bea551ef7dca9dbe8c51
Robson Lima <rsl.robson@gmail.com>
2022-09-24 16:07:35 -0300

    Creating Fragments and adding resources. - Setting up the Fragment Container for the MainActivity. - Creating the class and layout for the MainFragment. - Adding string resources.

--- app/src/main/java/com/robsonlima/simplemvcexample/view/MainFragment.kt
+++ app/src/main/java/com/robsonlima/simplemvcexample/view/MainFragment.kt
@@ -1,7 +1,20 @@
 package com.robsonlima.simplemvcexample.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.robsonlima.simplemvcexample.R
 
 class MainFragment: Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
 }
